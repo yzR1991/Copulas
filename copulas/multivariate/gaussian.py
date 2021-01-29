25ed425408b19871bd8fc3f5e0e0ab69a4567b0a
yzR1991 <49443945+yzR1991@users.noreply.github.com>
2021-01-29 12:04:45 +0800

    Update gaussian.py

--- copulas/multivariate/gaussian.py
+++ copulas/multivariate/gaussian.py
@@ -192,6 +192,7 @@ class GaussianMultivariate(Multivariate):
         samples = np.random.multivariate_normal(means, clean_cov, size=size)
         
         #对每一列，先用高斯分布生成随机数samples，再反过来用CDF生成百分数，然后生成对应分布的随机数
+        #如果要取百分数的话，cdf=np.linspace(0,1,20,endpoint=False)就行
         for i, (column_name, univariate) in enumerate(zip(self.columns, self.univariates)):
             cdf = stats.norm.cdf(samples[:, i])
             res[column_name] = univariate.percent_point(cdf)
